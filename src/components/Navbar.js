7bb8407448892ac921c4a01b693fed205c1030c1
Kat Grace <katherine.grace.reed@gmail.com>
2024-05-09 17:38:04 +0100

    Updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/react-icon-small.png" alt="React logo"/>
+            <img src="./react-icon-small.png" alt="React logo"/>
             <h3>ReactFacts</h3>
             <h4>React Course &mdash; Project 1</h4> 
         </nav>
